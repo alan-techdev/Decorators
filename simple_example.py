46ecfcdd4da9f214d8f00456116f9ec3be0b787a
Sherin-Hissen <sherin.techdev@gmail.com>
2026-06-06 03:46:26 +0300

    Add decorator to enhance function execution
    A simple example of decorators

---
+++ simple_example.py
@@ -0,0 +1,17 @@
+def myDecorator(func):   #Decorator
+  def nestedFunc():   # Any name it is for decoratiom
+       print("Before excuting the function")  # Message from decorator
+       func()   # Excute function
+       print("After excuting the function")   # Message from decorator
+  return nestedFunc  # return all data
+
+@myDecorator
+def sayHello():
+   print("Hello")
+
+@myDecorator
+def sayHowAreYou():
+   print("How are you?")
+
+sayHello()
+sayHowAreYou()
